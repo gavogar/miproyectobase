ae7ec6eb70412840be9740227fdbecef05262df9
gavogar <gustavogarcete@gmail.com>
2014-06-09 21:06:36 -0300

    se agregan js y cs

--- bootstrap/start.php
+++ bootstrap/start.php
@@ -26,7 +26,7 @@ $app = new Illuminate\Foundation\Application;
 
 $env = $app->detectEnvironment(array(
 
-	'local' => array('gavogar-VirtualBox'),
+	'local' => array('gavogar-Inspiron-N4030'),
 
 ));
 
